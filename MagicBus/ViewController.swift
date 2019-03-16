825e9f2a18ba76fada3a85a55048802c80ccbdfc
Alexander von Below <alexander.von_below@telekom.de>
2019-03-16 14:25:47 +0100

    Inital commit

---
+++ MagicBus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagicBus
+//
+//  Created by Alexander v. Below on 16.03.19.
+//  Copyright © 2019 Alexander von Below. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
